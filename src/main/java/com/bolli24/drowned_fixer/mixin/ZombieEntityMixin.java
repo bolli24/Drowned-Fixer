4efdf79def79c3a529d7924073d40d87b779a43d
bolli24 <4827765-bolli24@users.noreply.gitlab.com>
2025-11-02 00:11:35 +0100

    Update 1.1 - No longer fixes husks, zombie villagers and zombified piglin

--- src/main/java/com/bolli24/drowned_fixer/mixin/ZombieEntityMixin.java
+++ src/main/java/com/bolli24/drowned_fixer/mixin/ZombieEntityMixin.java
@@ -7,10 +7,15 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Mixin(ZombieEntity.class)
-public class ZombieEntityMixin {
+public abstract class ZombieEntityMixin {
+
     @Redirect(method = "damage(Lnet/minecraft/server/world/ServerWorld;Lnet/minecraft/entity/damage/DamageSource;F)Z",
             at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/mob/ZombieEntity;getType()Lnet/minecraft/entity/EntityType;"))
     private EntityType<? extends ZombieEntity> replaceEntityType(ZombieEntity instance) {
-        return EntityType.ZOMBIE;
+        EntityType<? extends ZombieEntity> actualType = instance.getType();
+        if (actualType == EntityType.DROWNED) {
+            return EntityType.ZOMBIE;
+        }
+        return actualType;
     }
 }
